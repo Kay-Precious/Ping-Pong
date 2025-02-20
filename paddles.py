3005517a1baada30f1c4cffcbf387879f004169d
Kay Precious <kayprecious977@gmail.com>
2025-02-20 12:28:00 +0100

    Update paddles.py

--- paddles.py
+++ paddles.py
@@ -1,11 +1,16 @@
 from turtle import Turtle
 
+
 class Paddle(Turtle):
+    '''Game Paddle Control'''
+
     def __init__(self, one_or_two):
         super().__init__()
 
-        if not one_or_two: self.one_or_two = one_or_two * 390
-        else: self.one_or_two = one_or_two * 380
+        if not one_or_two:
+            self.one_or_two = one_or_two * 390
+        else:
+            self.one_or_two = one_or_two * 380
         self.shape("square")
         self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
@@ -19,7 +24,3 @@ class Paddle(Turtle):
     def go_down(self):
         new_y = self.ycor() - 20
         self.goto(self.xcor(), new_y)
-
-
-
-
